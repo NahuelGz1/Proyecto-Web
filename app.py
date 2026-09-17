021104b7f854bd50c4aca382f034f436de115ffc
marcostiss <mtissera@fi.uba.ar>
2026-09-17 18:58:20 +0000

    cambios: esqueleto base, app.py, conexion db y modulo deportes actualizado con blueprints

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from src.constants import BASE_URL
+
+# se importan los blueprints a mano a medida que los creemos
+from src.routes.deportes import deportes_bp
+
+app = Flask(__name__)
+app.json.ensure_ascii = False
+
+# registra blueprints asociendoles la base_url
+app.register_blueprint(deportes_bp, url_prefix=BASE_URL)
+
+@app.route('/')
+def index():
+    return {"estado": "ok", "mensaje": "API Servidor Base Activo"}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
