8f0b955e6d8c7c80400a56bfa395b7acba020e43
NahuelGz1 <nlgomez@fi.uba.ar>
2026-09-19 03:08:15 -0300

    agrego validacion de body de nueva cancha casi completo

--- src/validators/canchas.py
+++ src/validators/canchas.py
@@ -1,3 +1,11 @@
+from src.constants import ERROR_CODE_INVALID_BODY
+from src.utils import (
+    validar_string_no_vacio,
+    construir_error_api,
+)
+
+
+
 class ValidationError(Exception):
     """Excepción personalizada para errores de validación en la solicitud."""
     pass
@@ -34,3 +42,63 @@ def validar_paginacion(args):
         raise ValidationError("_limit debe estar entre 1 y 100, y _offset no puede ser negativo")
 
     return limit, offset
+
+
+def validar_body_nueva_cancha(body: dict) -> dict:
+    """
+    valida el body del POST
+    Acepta nombre(nombre de cancha), id_deporte, precio_hora, techada, activa
+    """
+    if body is None:
+        raise ValueError(contruir_error_api(
+            code=ERROR_CODE_INVALID_BODY,
+            message='Cuerpo de la solicitud invalido',
+            description='El cuerpo debe ser un JSON valido con '
+
+        ))
+    errores = []
+
+    nombre = None
+    id_deporte   = None
+    precio_hora  = None
+    techada  = None
+    activa  = None
+
+
+    try:
+        nombre = validar_string_no_vacio(body.get('nombre'), 'nombre' )
+    except ValueError as e:
+        errores.extend(e.args[0]["errors"])
+
+    try:
+        id_deporte = validar_positivo(body.get('id_deporte'), 'id_deporte' )
+    except ValueError as e:
+        errores.extend(e.args[0]["errors"])
+
+    try:
+        precio_hora = validar_mayor_a_uno(body.get('precio_hora'), 'precio_hora' )
+    except ValueError as e:
+        errores.extend(e.args[0]["errors"])
+
+    """
+    validar techada?
+    """
+
+    try:
+        activa = validar_verdadero(body.get('activa'), 'activa' )
+    except ValueError as e:
+        errores.extend(e.args[0]["errors"])
+
+
+    if errores:
+        raise ValueError({'errors': errores})
+
+    return {
+        "nombre": nombre,
+        "id_deporte": id_deporte,
+        "precio_hora": precio_hora,
+        "techada": techada,
+        "activa": activa,
+
+    }
+
