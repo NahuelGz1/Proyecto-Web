9eb2c7bf494edf595019208eadb64bd8a3da2d7e
santigutal-ai <santigutal@gmail.com>
2026-09-22 14:49:29 -0300

    funcion insertar_socios terminada(repositories/socios)

--- src/repositories/socios.py
+++ src/repositories/socios.py
@@ -1,4 +1,4 @@
-from src.repositories.db import conexion
+from src.repositories.db import conexion, obtener_cursor
 
 
 def listar_socios(nombre, activo, limit, offset):
@@ -58,9 +58,19 @@ def listar_socios(nombre, activo, limit, offset):
 
 def insertar_socio(nombre: str, email: str, activo: bool) -> int:
     connection = conexion()
-    cursor = connection.cursor()
+    cursor = obtener_cursor(connection)
 
     try:
-        query = 
+        query = """
+            INSERT INTO socios (nombre, email, activo)
+            VALUES (%s, %s, %s);
+        """
+        cursor.execute(query, (nombre, email, activo))
+        connection.commit()
+        return cursor.lastrowid
+    finally:
+        cursor.close()
+        connection.close()
+
 
 
